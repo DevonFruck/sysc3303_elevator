b5fd7c24f0c17d9a002a6553c6188a75d3b8953a
Daniel Wang <Wangshujian1999@gmail.com>
2022-02-19 14:42:07 -0500

    updated ElevatorMotor
    updated ElevatorMotor

--- src/ElevatorMotor.java
+++ src/ElevatorMotor.java
@@ -1,16 +1,31 @@
-
+/**
+ * This class is controlling Elevator up/down directions
+ * @author ElevatorMotor
+ */
 public class ElevatorMotor {
 	
 	private motorStat status;
-	
+	/**
+	 * ElevatorMotor constructor
+	 * Defaults the elevator motor to idle 
+	 */
 	public ElevatorMotor() {
 		status = motorStat.IDLE;
 	}
 	
+	/**
+	 * getStatus
+	 * returns the status(enum) of elevator motor, up/down 
+	 * @return mortorStat
+	 */
 	public motorStat getStatus() {
 		return status;
 	}
 	
+	/**
+	 * setStatus
+	 * set the status(enum) of elevator motor, up/down 
+	 */
 	public void setStatus(motorStat status) {
 		this.status = status;
 	}
